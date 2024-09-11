0078f4b5a440a82dd13d0c7b8728ee1379435939
Daniel Svensson <danielsvenssonn@gmail.com>
2024-09-11 12:29:18 +0200

    second commit

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import HealthKit, {
+  HKQuantityTypeIdentifier,
+} from "@kingstinct/react-native-healthkit";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    async function setup() {
+      await HealthKit.requestAuthorization([
+        HKQuantityTypeIdentifier.stepCount,
+      ]);
+
+      const unsubscribe = HealthKit.subscribeToChanges(
+        HKQuantityTypeIdentifier.stepCount,
+        () => {
+          console.log("New sample available!");
+        }
+      );
+    }
+
+    setup();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
